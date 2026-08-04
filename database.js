603b6aede1cbaf97ab0b9f24bc6faca27e1527e7
Kaushalkurdiya <147405866+Kaushalkurdiya@users.noreply.github.com>
2026-08-04 23:04:02 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose'; 
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Atlas connected successfully');
+  } catch (err) {
+    console.error('Connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ web_service.js
@@ -0,0 +1,21 @@
+import express from 'express'; 
+import connectDB from './database.js';
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express(); 
+connectDB();
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}); 
