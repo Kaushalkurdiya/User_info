7492eb0024e4c7f33ae51bf81694ef32f228ea75
Kaushalkurdiya <147405866+Kaushalkurdiya@users.noreply.github.com>
2026-08-04 23:08:11 +0530

    Update web_service.js

--- web_service.js
+++ web_service.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 }); 
